fb4557330645c85745563c59f16a5cd5aed94759
sziir <moode1121134@gmail.com>
2023-07-09 20:02:03 +0300

    most sold product

--- src/index.js
+++ src/index.js
@@ -3,4 +3,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/style.css';
 import '@fortawesome/fontawesome-free/js/all.min';
 
-document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(item => new bootstrap.Tooltip(item))
+document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(item => new bootstrap.Tooltip(item));
+
+
+document.querySelectorAll('.add-to-cart-btn').forEach(item => {
+    item.addEventListener('click', () => {
+        alert('أضيف المنتج إلى عربة الشراء')
+    })
+})
+ 
+
