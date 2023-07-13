2917e61fba3738a0f35a9fd38ffd479cf6b1e8c8
sziir <moode1121134@gmail.com>
2023-07-13 11:39:29 +0300

    product page done

--- src/index.js
+++ src/index.js
@@ -8,8 +8,24 @@ document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(item => new boot
 
 document.querySelectorAll('.add-to-cart-btn').forEach(item => {
     item.addEventListener('click', () => {
-        alert('أضيف المنتج إلى عربة الشراء')
-    })
-})
- 
+        alert('أضيف المنتج إلى عربة الشراء');
+    });
+});
+document.getElementById('year').innerHTML = new Date().getFullYear();
 
+document.querySelectorAll('.size-option input[type="radio"]').forEach(item => {
+    item.addEventListener('change', () => {
+        document.querySelectorAll('.size-option').forEach(i => {
+            i.classList.remove('active');
+        });
+        item.parentNode.parentNode.classList.add('active');
+    });
+});
+document.querySelectorAll('.color-option input[type="radio"]').forEach(item => {
+    item.addEventListener('change', () => {
+        document.querySelectorAll('.color-option').forEach(i => {
+            i.classList.remove('active');
+        });
+        item.parentNode.parentNode.classList.add('active');
+    });
+});
